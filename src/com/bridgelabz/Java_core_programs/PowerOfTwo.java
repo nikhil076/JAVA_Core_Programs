7d52a571db72327f0c47e518adf53096ada9f84f
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-23 13:37:12 +0530

    added the power of 2 table program

---
+++ src/com/bridgelabz/Java_core_programs/PowerOfTwo.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.Java_core_programs;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number till the power of 2 required :");
+		int N = sc.nextInt();
+		int POWER_OF_TWO = 1;
+		if(0<=N && N<31)
+		{
+			for(int i = 1; i<=N;i++)
+			{
+				POWER_OF_TWO = POWER_OF_TWO * 2;
+				System.out.println(i+" "+POWER_OF_TWO);
+			}
+		}
+		else
+		{
+			System.out.println("overflow of integer type");
+		}
+	}
+}
