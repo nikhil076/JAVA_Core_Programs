f386e3e486b58e528e0fa8e567df6f00a390bfab
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-23 21:27:06 +0530

    added the factors programs

---
+++ src/com/bridgelabz/Java_core_programs/Factors.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.java_core_programs;
+
+import java.util.Scanner;
+
+public class Factors {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number to check for its prime factors");
+		int N = sc.nextInt();
+		
+		for(int i = 2; i< N; i++) {
+	         while(N%i == 0) {
+	            System.out.println(i+" ");
+	            N = N/i;
+	         }
+	      }
+	      if(N >2) {
+	         System.out.println(N);
+	      }
+	}
+
+}
